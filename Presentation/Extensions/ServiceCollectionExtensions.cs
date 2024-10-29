02d1691b7ac609e2e251fd264b1ea2d61387e3c5
Diogo L Scarmagnani <diogols@live.com>
2024-10-29 19:48:56 -0300

    ahhhhhhhhhhhh

--- Presentation/Extensions/ServiceCollectionExtensions.cs
+++ Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -218,11 +218,11 @@ public static class ServiceCollectionExtensions
 
                 // Garante que os cookies sejam enviados apenas em conexões HTTPS, o que é ótimo para segurança.
                 // Porém aqui foi deixado como 'None' pois isso atrapalha em conexões com localhost.
-                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-                // options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+                // options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.None;
 
                 // // Necessário para cross-origin
-                options.Cookie.SameSite = SameSiteMode.Lax;
+                options.Cookie.SameSite = SameSiteMode.None;
 
                 // options.Cookie.SameSite = isDevelopment
                 //     ? SameSiteMode.Lax
